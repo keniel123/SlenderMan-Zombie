02d8cba1422d988b81c07d88be9ea2a1f18c8bc6
Keniel Peart <keniel_peart123@hotmail.com>
2017-05-24 12:05:21 -0500

    zombie runner

---
+++ Assets/Zombie.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour {
+	AudioSource audioSource;
+	public AudioClip comeHere;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+
+		audioSource.clip = comeHere;
+		audioSource.Play ();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+			
+		
+		} 
+
+
+
+
+
+}
---
+++ Assets/landprefab.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class landprefab : MonoBehaviour {
+	public float timeSince = 0f;
+	AudioSource audioSource;
+	public AudioClip zombieSpawn;
+	public AudioClip zombiechase;
+	bool alreadyPlaying = false;
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.clip = zombieSpawn;
+		audioSource.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeSince += Time.deltaTime;
+
+		if (alreadyPlaying == false && timeSince > 10f) {
+			audioSource.spatialBlend = 1.0f;
+			audioSource.minDistance = 20;
+			audioSource.maxDistance = 650;
+			audioSource.clip = zombiechase;
+			audioSource.loop = true;
+			audioSource.Play ();
+			alreadyPlaying = true;
+
+		}
+	}
+}
